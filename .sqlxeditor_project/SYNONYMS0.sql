055a8aaa9640e5c26e8d50131233315afb548087
akil-rails <kataria.subs@gmail.com>
2014-08-21 20:46:20 +0530

    new: the initial commit for the message flow that will process the openGroupLoanAccountRequest

---
+++ .sqlxeditor_project/SYNONYMS0.sql
@@ -0,0 +1,6 @@
+create synonym [HO-PROCESS-01] for ganaseva_uat.dbo.[HO-PROCESS-01];
+create synonym [HO-PROCESS-02] for ganaseva_uat.dbo.[HO-PROCESS-02];
+create synonym [HO-PROCESS-04] for ganaseva_uat.dbo.[HO-PROCESS-04];
+create synonym [HO-PROCESS-06] for ganaseva_uat.dbo.[HO-PROCESS-06];
+create synonym [HO-PROCESS-A0] for ganaseva_uat.dbo.[HO-PROCESS-A0];
+create synonym [HO-PROCESS-CU] for ganaseva_uat.dbo.[HO-PROCESS-CU];
